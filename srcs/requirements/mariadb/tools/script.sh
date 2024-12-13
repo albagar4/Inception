5fba0ce9635918b1e451bd03ab838f578d34ff91
Alba <albagar4@student.42malaga.com>
2024-12-13 17:44:21 +0100

    nginx doesn't work

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+mysqld &
 
-service mysql start;
-
+sleep 1
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
@@ -10,4 +10,4 @@ mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-exec mysqld_safe
+exec mysqld
